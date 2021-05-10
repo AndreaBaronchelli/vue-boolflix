4ee8548fbb0343fd0921009918d999689a4e4d6f
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-05-10 22:47:07 +0200

    add flags

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,18 @@
         <ul>
             <li>Titolo: {{ info.title }}</li>
             <li>Titolo Originale: {{ info.original_title }}</li>
-            <li>Lingua Originale: {{ info.original_language }}</li>
+            <li v-if="!flagsArray.includes(info.original_language)">
+                Lingua Originale: {{ info.original_language }}
+            </li>
+            <li v-else>
+                <span>Lingua Originale:</span>
+                <img
+                    :src="
+                        require(`../assets/img/${info.original_language}.png`)
+                    "
+                    alt=""
+                />
+            </li>
             <li>Voto: {{ info.vote_average }}</li>
         </ul>
     </div>
@@ -13,6 +24,11 @@
 export default {
     name: "Card",
     props: ["info"],
+    data() {
+        return {
+            flagsArray: ["en", "it"],
+        };
+    },
 };
 </script>
 
@@ -21,5 +37,9 @@ export default {
     width: 300px;
     margin-right: 50px;
     margin-bottom: 50px;
+    img {
+        width: 20px;
+        margin-left: 5px;
+    }
 }
 </style>
